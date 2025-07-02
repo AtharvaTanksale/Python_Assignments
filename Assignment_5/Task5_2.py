f455b9a2cb096b877d6985567cd82102b925d962
Atharva Tanksale <atharvat2506@gmail.com>
2025-07-02 20:47:59 +0530

    Add files via upload

---
+++ Assignment_5/Task5_2.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+print(f"Original list:{list}")
+sublist = list[0:5]
+print(f"extracted list is : {sublist}")
+sublist.reverse()
+print(f"Reversed Extracted List is: {sublist}")
